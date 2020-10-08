8bab476b3ac9b270c3e099d9214fb472713408ab
Risky Febiansyah <riskifebiansyah@hotmail.com>
2020-10-08 13:49:59 +0700

    Adding search function

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
 const app = express();
+const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 5000;
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+//API key pk_84b9c432f0ad4c0aa58c587d3f3fdee9
+// create call_api function
+function call_api(finishedAPI, ticker){
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_84b9c432f0ad4c0aa58c587d3f3fdee9', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		}
+	});
+}
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -11,11 +28,28 @@ app.set('view engine', 'handlebars');
 
 var stup = "Other stuff";
 
-// Set handlebars routes
+// Set handlebars index Get routes
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: stup
-    });
+    call_api( function(doneAPI){
+    	res.render('home', {
+    	stock: doneAPI
+    	});
+    }, "fb");
+});
+
+// Set handlebars index POST routes
+app.post('/', function (req, res) {
+    call_api( function(doneAPI){
+    	//posted_stuff = req.body.stock_ticker;
+    	res.render('home', {
+    	stock: doneAPI,
+    	});
+    }, req.body.stock_ticker);
+});
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 //set static folder
